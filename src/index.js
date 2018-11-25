38218764289706e9af3c6d5da13949e3942f5b21
Maks <maks.lisogyrskyi@gmail.com>
2018-11-25 16:55:56 +0200

    ready

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import './styles/style.css';
 import $ from 'jquery';
+
+let textIn = $('.message-field');
+
+let textOut = $('.message-formatted');
+
+textIn.addEventListener('input', function () {
+
+    textOut.innerHTML = textIn.value.toLowerCase().replace(/^\s+/, '').replace(/\n/g, ' ');
+
+});
