2b25d06da0b47ea8b86e6c32e3974dbc8df4d73e
Maks <maks.lisogyrskyi@gmail.com>
2018-11-28 12:10:11 +0200

    add the new function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import './styles/style.css';
+
 import $ from 'jquery';
 
-let textIn = $('.message-field');
+let textIn = document.jquerySlector('.message-field');
 
-let textOut = $('.message-formatted');
+let textOut = document.jquerySlector('.message-formatted');
+
+// textIn.addEventListener('input', function () {
+
+// 	textOut.innerHTML = textIn.value.toLowerCase().trim().replace(/^\s+/, '').replace(/\n/g, ' ');
+
+// });
+
+function formattedMessage () {
+	let spaceSearch = /^\s+\n/g;
+
+	return textIn.value.toLowerCase().trim().replace(spaceSearch, '');
+}
 
 textIn.addEventListener('input', function () {
-
-    textOut.innerHTML = textIn.value.toLowerCase().replace(/^\s+/, '').replace(/\n/g, ' ');
-
+	textOut.innerHTML(formattedMessage);
 });
